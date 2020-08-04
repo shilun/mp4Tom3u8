e1e2b5d5810bfcfa5b3ad5f674a9d83969968ae4
mac <xxx@qq.copm>
2020-08-04 20:00:23 +0800

    Revert "init"
    This reverts commit ae1364da9a73bcb0197eed1d04a93a647560dd41.
    
    # Conflicts:
    #	src/app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -14,9 +14,10 @@ export class AppController {
     let path = source.substr(0, source.indexOf('.'));
     let name = source.substr(source.lastIndexOf('/') + 1).replace('.mp4', '');
     let tempFile = path + '/.' + name + '.mp4';
-    exec('ffmpeg -i ' + source + ' -metadata rotate="" ' + tempFile);
-    exec('ffmpeg -i ' + source + ' -codec copy -vbsf h264_mp4toannexb -map 0 -f segment -segment_list ' + path + '/default.m3u8 -segment_time  2 ' + path + '/%03d.ts');
-    fs.unlinkSync(tempFile);
+    exec('ffmpeg -i ' + source + ' -metadata rotate="" ' + tempFile,(err, stdout, stderr)=>{
+      exec('ffmpeg -i ' + tempFile + ' -codec copy -vbsf h264_mp4toannexb -map 0 -f segment -segment_list ' + path + '/default.m3u8 -segment_time  2 ' + path + '/%03d.ts');
+      fs.unlinkSync(tempFile);
+    });
     return { status:true };
   }
 }
